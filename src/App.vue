229e047027ebf1210a3847a80500f905126e31aa
cyebukayire <peacecyebukayire@gmail.com>
2022-01-22 18:52:12 +0200

    Created Joining page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Join/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Join from './components/Join.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Join
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  /* background: #e6dffa; */
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Join.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="join">
+         <form class="box" method="">
+      <h1>Dev C</h1>
+      <input type="text" name="" placeholder="Username">
+      <input type="submit" value="Join">
+    </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Join',
+}
+</script>
+
+<style scoped>
+/* .join{
+    position: relative;
+    width: 100vmin;
+    height: 100vmin;
+    top: 0;
+    left: 0;
+} */
+
+
+.box{
+  width: 300px;
+  padding: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  background: #011c35;
+  text-align: center;
+  border-radius: 20px 20px;
+}
+.box h1{
+  color: #87bfff;
+  /* text-transform: uppercase; */
+  font-weight: 500;
+  text-align: center;
+}
+.box input[type = "text"],.box input[type = "password"]{
+  border:0;
+  background: none;
+  display: block;
+  margin: 5px auto;
+  text-align: center;
+  border: 2px solid white;
+  padding: 14px 10px;
+  width: 200px;
+  outline: none;
+  color: white;
+  border-radius: 24px;
+  transition: 0.5s;
+}
+.box input[type = "text"]:focus{
+  border-color: #87bfff;
+  width: 250px;
+}
+.box input[type = "submit"]{
+  border:0;
+  background: none;
+  display: block;
+  margin: 20px auto;
+  text-align: center;
+  border: 2px solid white;
+  padding: 14px 40px;
+  outline: none;
+  color: white;
+  transition: 0.25s;
+  cursor: pointer;
+}
+.box input[type = "submit"]:focus{
+  border-color: #87bfff;
+}
+.box input[type = "submit"]:hover{
+  /* background: #D4AC0D; */
+  border-radius: 24px;
+}
+
+
+</style>
