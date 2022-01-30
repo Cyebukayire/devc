bb3414659a1187ef5c63d1b711387c6ce9256ce0
cyebukayire <peacecyebukayire@gmail.com>
2022-01-30 16:07:00 +0200

    fixed the bug

--- src/components/CreateMessage.vue
+++ src/components/CreateMessage.vue
@@ -1,74 +1,69 @@
 <template>
-    
-		<footer>
-			<!-- HELLO T -->
-			<textarea placeholder="Type your message" v-model="state.messageContent"></textarea>
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="">
-			<!-- <a href="#">Send</a> -->
-			<!-- <button>SEND</button> -->
-			<!-- {{state.messageContent}} -->
-
-			<!-- Working on Word Counter in the textarea -->
-		</footer>
-
+  <footer>
+    <!-- HELLO T -->
+    <textarea placeholder="Type your message" v-model="state.messageContent" />
+    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt=""/>
+    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt=""/>
+    <a href="#">Send</a>
+    <button>SEND</button>
+  </footer>
 </template>
 
 <script>
-import { reactive } from 'vue'
-export default {
-	name: "CreateMessage",
-	setup() {
-		const state = reactive({
-			messageContent: '',
-		})
-		
-	return {
-		state,
-	}
+import {defineComponent, ref} from "vue";
 
-	},
-}
+export default defineComponent({
+  name: "CreateMessage",
+  setup() {
+    const state = ref<{messageContent: String}>({
+      messageContent: "",
+    });
+
+    return { state };
+  },
+})
 </script>
-<style scoped>
 
+
+<style scoped>
 /* Footer */
-footer{
-	height:155px;
-	padding:20px 30px 10px 20px;
+footer {
+  height: 155px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 20px 30px 10px 20px;
 }
-footer textarea{
-	resize:none;
-	border:none;
-	display:block;
-	width:100%;
-	height:80px;
-	border-radius:3px;
-	padding:20px;
-	font-size:16px;
-	margin-bottom:13px;
+footer textarea {
+  resize: none;
+  border: none;
+  display: block;
+  width: 80%;
+  height: 80px;
+  border-radius: 3px;
+  padding: 20px;
+  font-size: 16px;
+  margin-bottom: 13px;
 }
 footer textarea:focus {
-	outline: none !important;
-	border: 2px solid #cae5ff;
-	
+  outline: none !important;
+  border: 2px solid #cae5ff;
 }
-footer textarea::placeholder{
-	color:rgb(153, 156, 158);
-	font-size: 16px;
+footer textarea::placeholder {
+  color: rgb(153, 156, 158);
+  font-size: 16px;
 }
-footer img{
-	height:30px;
-	cursor:pointer;
+footer img {
+  height: 30px;
+  cursor: pointer;
 }
-footer a{
-	text-decoration:none;
-	text-transform:uppercase;
-	font-weight:bold;
-	color:#6fbced;
-	vertical-align:top;
-	margin-left:333px;
-	margin-top:5px;
-	display:inline-block;
+footer a {
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #6fbced;
+  vertical-align: top;
+  margin-top: 5px;
+  display: inline-block;
 }
 </style>
